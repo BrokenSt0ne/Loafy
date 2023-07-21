7e39c7047e1a3cec6379f78e816574c7ab573b74
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2023-07-20 21:43:15 -0400

    PluginInfo and patching
    Plugin info to make the mod a little cleaner and less congested and a rewrite of the player controller so scaling works and just to make modding the game a bit easier when it comes to the player controller

---
+++ PluginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using BepInEx;
+
+namespace Loafy
+{
+    public class PluginInfo
+    {
+        public const string GUID = "Pheonix.Loafy";
+        public const string Name = "Loafy";
+        public const string Version = "1.0.1";
+    }
+}
