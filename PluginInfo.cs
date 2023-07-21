ff71b735df197028116b74f501b37989fac7f9db
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2023-07-21 19:02:32 -0400

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -9,6 +9,6 @@ namespace Loafy
     {
         public const string GUID = "Pheonix.Loafy";
         public const string Name = "Loafy";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
     }
 }
